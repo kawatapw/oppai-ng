a86f11469528f42077361414759b56e4ff4588d0
Giuseppe Guerra <to@nyo.zz.mu>
2021-06-19 20:02:38 +0200

    Tagged manylinux docker images

--- swig/python/build.sh
+++ swig/python/build.sh
@@ -4,7 +4,7 @@ rm -rf ./dist
 cp ../../oppai.c .
 cp ../oppai.i .
 swig -python -includeall oppai.i || exit
-for img in quay.io/pypa/manylinux2010_x86_64 quay.io/pypa/manylinux2010_i686; do
+for img in quay.io/pypa/manylinux2010_x86_64:2021-05-01-28d233a quay.io/pypa/manylinux2010_i686:2021-05-01-28d233a; do
   docker run --user 1000:1000 --rm -v $(pwd):/io -w /io $img \
     ./build_wheels.sh || exit
 done
